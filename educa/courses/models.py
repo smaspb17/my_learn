7c54d75831f54df7ce8e1d5fe4199c53ec7e60e5
Marat Shaibakov <smaspb17@yandex.ru>
2024-07-23 12:11:28 +0300

    Create courses app, Subject/Course/Module models, models of polymorphic content - Content (ContentType)/ItemBase/Text/File/Image/Video.

---
+++ educa/courses/models.py
@@ -0,0 +1,161 @@
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
+
+User = get_user_model()
+
+
+class Subject(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='courses_created',
+        on_delete=models.CASCADE,
+        verbose_name='Создатель'
+    )
+    subject = models.ForeignKey(
+        Subject,
+        related_name='courses',
+        on_delete=models.CASCADE,
+        verbose_name='Предмет',
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Слаг'
+    )
+    overview = models.TextField(
+        verbose_name='Описание',
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Создан'
+    )
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(
+        Course,
+        related_name='modules',
+        on_delete=models.CASCADE,
+        verbose_name='Курс'
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание'
+    )
+    # order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
+        # ordering = ['order']
+
+    def __str__(self):
+        return {self.title}
+
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE,
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in': (
+                'text',
+                'video',
+                'image',
+                'file',
+            )
+        }
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey(
+        'content_type',
+        'object_id'
+    )
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=250,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(
+        upload_to='files',
+    )
+
+
+class Image(ItemBase):
+    file = models.ImageField(
+        upload_to='images',
+    )
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
+
