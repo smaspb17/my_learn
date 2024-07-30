4cceaee4a18505d9fee16a71f8f6173f529b0b95
Marat Shaibakov <smaspb17@yandex.ru>
2024-07-30 19:44:22 +0300

    Added public display area of the courses and custom filter pluralize_ru.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -3,15 +3,17 @@ from django.apps import apps
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
+from django.db.models import Count
 from django.forms.models import modelform_factory
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from courses.forms import ModuleFormSet
-from courses.models import Course, Module, Content
+from courses.models import Course, Module, Content, Subject
 
 
 def logout_user(request):
@@ -192,6 +194,32 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
                 content__owner=request.user
             ).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses
+        })
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -4,13 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from courses.views import logout_user
+from courses.views import logout_user, CourseListView
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', logout_user, name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
