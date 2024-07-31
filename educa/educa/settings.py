3396321d83051b86fe386915a6903e2121a168ae
Marat Shaibakov <smaspb17@yandex.ru>
2024-07-31 22:31:24 +0300

    Added REST API by DRF, serializers, custom sets, views, permissions, routers, also added authentication, requests library to consume the API from an external Python script.

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,10 @@ CACHES = {
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 минут
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('students/', include('students.urls')),
     path('', CourseListView.as_view(), name='course_list'),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api/', include('courses.api.urls', namespace='api'))
 ]
 
 if settings.DEBUG:
